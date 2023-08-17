4b9136a88a53ac3f07f52cd48bf70f5e7002afdb
nathbalica <nathbalica@gmail.com>
2023-08-17 09:19:09 -0300

    feat: :sparkles: routes auth done

--- src/App.js
+++ src/App.js
@@ -1,29 +1,24 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import styled from "styled-components"
-// import HomePage from "./pages/HomePage"
+import HomePage from "./pages/HomePage/HomePage"
 import LoginPage from "./pages/LoginPage"
 import SignUpPage from "./pages/SignUpPage"
-import AuthContext from "./contexts/AuthContext"
-import { useState } from "react"
-// import mainColor from "./constants/colors"
+import { AuthContextProvider } from "./contexts/AuthContext"
 
 
 export default function App() {
-  const [token, setToken] = useState(localStorage.getItem("token"))
-  const [userName, setUserName] = useState(localStorage.getItem("userName"))
-
   return (
 
-      <AuthContext.Provider value={{ token, setToken, userName, setUserName }}>
+      <AuthContextProvider>
         <BrowserRouter>
           <Routes>
             <Route path="/" element={<LoginPage />} />
             <Route path="/cadastro" element={<SignUpPage />} />
-            {/* <Route path="/home" element={<HomePage />} /> */}
+            <Route path="/home" element={<HomePage />} />
 
           </Routes>
         </BrowserRouter>
-      </AuthContext.Provider>
+      </AuthContextProvider>
 
 
   )
